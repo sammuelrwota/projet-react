7b33f7d74c97134b8ec08f4ba55e4a64c6d223b1
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-02-25 14:13:46 -0500

    changements

--- src/Composants/MonProfile/Profile.jsx
+++ src/Composants/MonProfile/Profile.jsx
@@ -5,7 +5,7 @@ function Profile({ setActivePage }) {
     return (
         <div className="principal">
             <img src="/images/PhotoDeMoi.jpg" alt="Photo de l'étudiant" style={{ width: '150px', height: '150px' }} />
-            <h1><span>Je suis Sammuel Rwota </span>étudiant à Maisonneuve!</h1>
+            <h1><span>Je suis Sammuel Rwota </span>étudiant à Maisonneuve.</h1>
             <p>En recherche d'emploi d'été.</p>
             <div className="cv">
                 <div className="connecte-avec-moi" onClick={() => setActivePage('connecte')}>Connecte avec moi</div>
