fa8c9f4099bbba164e2eefeee1454f913c5b574f
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-03-03 00:52:01 -0500

    Mise a jour de modification

--- src/data/mestravaux.js
+++ src/data/mestravaux.js
@@ -18,7 +18,19 @@ const mestravaux_data = [
     w_no: 4,
     w_name: "Web",
     w_img: "images/image4.png"
-  }
+  }, 
+  {
+  w_no: 5,
+  w_name: "Web",
+  w_img: "images/image4.png"
+  },
+  {
+    w_no: 6,
+    w_name: "Web",
+    w_img: "images/image4.png"
+    }
+
+  
 ];
 
 export default { mestravaux_data };
