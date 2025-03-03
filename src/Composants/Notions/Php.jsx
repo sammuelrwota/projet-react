719fcbc6cfae587423935c6d552c63434899b9ce
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-03-03 13:03:37 -0500

    Modifications (notions)

--- src/Composants/Notions/Php.jsx
+++ src/Composants/Notions/Php.jsx
@@ -8,7 +8,7 @@ function Php() {
             <h1>PHP</h1>
             <div className='paragraphePhp'>
                 <p className='pPhp'>  
-                PHP m'a beaucoup aidé à comprendre le back-end, car il permet de créer des applications web dynamiques et interactives en manipulant des bases de données et en générant du contenu côté serveur. J'ai aimé apprendre l'utilisation de XAMPP avec MySQL et Apache, ce qui m'a permis de comprendre le rôle de chaque composant dans le développement web. PHP me permet de créer des pages web dynamiques en traitant les données envoyées par les utilisateurs, en les stockant dans une base de données MySQL et en affichant des informations en temps réel. Ce cours m'aide à bâtir un site via PHP, à intégrer les données dans la base de données, et à comprendre le fonctionnement des requêtes SQL pour interagir avec ces données de manière sécurisée et optimisée. Grâce à cette expérience, je suis désormais plus à l’aise dans la création de sites web complets, intégrant à la fois la partie front-end et back-end.
+                PHP m'a aidé à comprendre le back-end en créant des sites web dynamiques avec MySQL et Apache via XAMPP. Il permet de gérer les bases de données, traiter les données des utilisateurs et afficher du contenu en temps réel. Ce cours m'a appris à intégrer des données, utiliser des requêtes SQL sécurisées et optimiser le développement web. Grâce à cette expérience, je me sens plus à l'aise pour créer des sites complets en front-end et back-end.
                 </p>
             </div>
         </div>
--- src/Composants/Notions/Wp.jsx
+++ src/Composants/Notions/Wp.jsx
@@ -8,7 +8,7 @@ function Wp() {
                 <h1>WordPress</h1>
                 <div className='paragrapheWp'>  
                     <p className='pWp'>  
-                    Avec mon enseignant Eddy Martin, je poursuis mon apprentissage de WordPress, un puissant gestionnaire de contenu qui facilite la création et la gestion de sites web dynamiques. En utilisant WordPress, j’approfondis mes compétences en développement de thèmes personnalisés, ce qui me permet de répondre aux besoins spécifiques du design d’interfaces web. J'explore également les fonctionnalités avancées de WordPress, notamment la programmation d'extensions pour enrichir les fonctionnalités des sites web que je développe. De plus, j'ai été initié à l’utilisation et à l'intégration des bases de données via WordPress, ce qui me permet de gérer efficacement le contenu dynamique des sites. Cette expérience m'offre une compréhension approfondie des aspects techniques de WordPress, essentiels pour créer des sites web interactifs et performants.
+                    Avec mon enseignant Eddy Martin, j'apprends à utiliser WordPress pour créer et gérer des sites web dynamiques. Je développe des thèmes personnalisés, programme des extensions et intègre des bases de données pour gérer le contenu. Cette expérience me permet de maîtriser les aspects techniques de WordPress et de créer des sites interactifs et performants.
                     </p>
                 </div>
             </div>
