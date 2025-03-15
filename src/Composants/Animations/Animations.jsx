6faea261a82ae28751ca4c552f348a10ebb1d669
Sammuel Rwota <e2289486@cmaisonneuve.qc.ca>
2025-03-14 20:43:57 -0400

    Structure des composants modifiés.

--- src/Composants/Animations/Animations.jsx
+++ src/Composants/Animations/Animations.jsx
@@ -6,14 +6,14 @@ function Animations() {
   const { t } = useTranslation();  
 
   return (
-    <div className="content">
-      <div className="content__container">
-        <p className="content__container__text">{t('animation_title')}</p> 
-        <ul className="content__container__list">
-          <li className="content__container__list__item">{t('recent')}</li>  
-          <li className="content__container__list__item">{t('practical')}</li> 
-          <li className="content__container__list__item">{t('old')}</li> 
-          <li className="content__container__list__item">{t('personal')}</li> 
+    <div className="contenu">
+      <div className="contenu__conteneur">
+        <p className="contenu__conteneur__texte">{t('animation_title')}</p> 
+        <ul className="contenu__conteneur__liste">
+          <li className="contenu__conteneur__liste__element">{t('recent')}</li>  
+          <li className="contenu__conteneur__liste__element">{t('practical')}</li> 
+          <li className="contenu__conteneur__liste__element">{t('old')}</li> 
+          <li className="contenu__conteneur__liste__element">{t('personal')}</li> 
         </ul>
       </div>
     </div>
